31f19d5a371d307767cd95c9edec5249a6e051c4
uttamkrshaw <uttamkr5599@gmail.com>
2025-05-24 23:44:11 +0530

    Working on Backend

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import 'dotenv/config'
+
+import express from 'express';
+import cors from 'cors';
+import { createServer } from "http";
+import { connectDB } from './config/db.js';
+const app = express();
+const server = createServer(app);
+
+app.use(express.json())
+// const io = new Server(server, {
+//     cors: {
+//         origin: process.env.clientUrl,
+//         methods: ["GET", "POST", "PATCH", "DELETE"],
+//         credentials: true
+//     }
+// });
+
+app.use(cors({
+    origin: process.env.clientUrl,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true
+}))
+
+
+
+
+server.listen(process.env.PORT, async () => {
+    try {
+        await connectDB();
+        console.log(`Server Started On PORT ${process.env.PORT}`);
+    } catch (error) {
+        console.log(`Failed To Run Server. Error Reason :- ${error.message}`);
+
+    }
+})
